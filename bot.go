f28eee2fc80ae2efcf9757eb0075b79e59672c7d
slab713 <daninhoseven@gmail.com>
2022-08-31 02:50:32 +0200

    Initial commit

--- bot.go
+++ bot.go
@@ -45,11 +45,21 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 
 	if m.Content == "/flagz" {
-		num := pickRandomNr()
-		fmt.Println(num)
+		initialRandomNum := pickRandomNr()
+		fmt.Println(initialRandomNum)
 	}
 }
 
-/*	pick a random file from the slice, display it in the chat, add the multiple choice menu, somehow add right and wrong answers,
-	let user select and enter ... msg in the chat if it's right or wrong, sleep for a few seconds, remove old image from slice,
-	repeat from the start */
+/* TODO?
+
+1) Get a copy of the complete, original map from the cache
+2) If the initialRandomNum is e.g. 197, pick the respective value (in this case: images\z\zimbabwe.png) and post this image in the chat (os.Open? os.Read? How to do it with Discordgo?)
+3) Somehow create a select menu fitting to this posted national flag --> A: South Africa, B: Zimbabwe, C: Iran, D: USA (for example).
+   Where to store these options for 197 flags and how to include them each time? Because for other flags (e.g. 1:images\a\afghanistan.png), I want other select menu options.
+4) Inform the player if their answer was wrong or right. Or if better, just post the right answer without checking via the select menu.
+5) Delete the already used image from the map and sleep a few seconds (so the user can read the result)
+6) Pick a new random number (... create another function: pick random number from current len() of the map copy)
+7) Edit the Message, show new image, select menu etc.
+8) Repeat until map is empty
+
+*/
